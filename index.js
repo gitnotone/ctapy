9be8824f74d5d70643258dd62a880edf8a019e4c
eric <hw1208.ahn@gmail.com>
2017-10-12 21:23:34 +0900

    module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//index.js
+
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
@@ -5,7 +7,7 @@ var methodOverride = require("method-override");
 var app = express();
 
 // DB setting
-mongoose.connect(process.env.MONGO_DB, { useMongoClient: true }); // 1
+mongoose.connect(process.env.MONGO_DB, { useMongoClient: true });
 var db = mongoose.connection;
 db.once("open", function(){
  console.log("DB connected");
@@ -21,69 +23,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
-
-// DB schema
-var contactSchema = mongoose.Schema({
- name:{type:String, required:true, unique:true},
- email:{type:String},
- phone:{type:String}
-});
-var Contact = mongoose.model("contact", contactSchema); //5
-
 // Routes
-// Home
-app.get("/", function(req, res){
- res.redirect("/contacts");
-});
-// Contacts - Index
-app.get("/contacts", function(req, res){
- Contact.find({}, function(err, contacts){
-  if(err) return res.json(err);
-  res.render("contacts/index", {contacts:contacts});
- })
-});
-// Contacts - New
-app.get("/contacts/new", function(req, res){
- res.render("contacts/new");
-});
-// Contacts - create
-app.post("/contacts", function(req, res){
- Contact.create(req.body, function(err, contact){
-  if(err) return res.json(err);
-  res.redirect("/contacts");
- });
-});
-// Contacts - show // 3
-app.get("/contacts/:id", function(req, res){
- Contact.findOne({_id:req.params.id}, function(err, contact){
-  if(err) return res.json(err);
-  res.render("contacts/show", {contact:contact});
- });
-});
-// Contacts - edit // 4
-app.get("/contacts/:id/edit", function(req, res){
- Contact.findOne({_id:req.params.id}, function(err, contact){
-  if(err) return res.json(err);
-  res.render("contacts/edit", {contact:contact});
- });
-});
-// Contacts - update // 5
-app.put("/contacts/:id", function(req, res){
- Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
-  if(err) return res.json(err);
-  res.redirect("/contacts/"+req.params.id);
- });
-});
-// Contacts - destroy // 6
-app.delete("/contacts/:id", function(req, res){
- Contact.remove({_id:req.params.id}, function(err, contact){
-  if(err) return res.json(err);
-  res.redirect("/contacts");
- });
-});
+app.use("/", require("./routes/home"));
+app.use("/contacts", require("./routes/contacts"));
 
 // Port setting
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
- console.log("server on!");
+ console.log("server on");
 });
